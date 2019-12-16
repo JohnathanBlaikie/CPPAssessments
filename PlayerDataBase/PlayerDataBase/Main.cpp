fcadd2c540e756b32f5b32dd734fc5ba7c8497e6
Johnathan Blaikie <johnathanblaikie@gmail.com>
2019-12-16 11:36:27 -0800

    Sike

--- PlayerDataBase/PlayerDataBase/Main.cpp
+++ PlayerDataBase/PlayerDataBase/Main.cpp
@@ -11,24 +11,25 @@ int main() {
 	bool loop = true;
 	char fN[64];
 	char lN[64];
-	int age = 0, maxID = 64;;
+	int age = 0, maxID = 64;
 	char *fND = new char[maxID];
 	char *lND = new char[maxID];
 	
-
-	while (loop) {
-		cout << "\nPlease enter your first name:\n";
-		cin >> fN;
-		cout << "\nPlease enter your last name:\n";
-		cin >> lN;
-		cout << "\nPlease enter your age:\n";
-		cin >> age;
-		UserDefData* uDD = new UserDefData(fN, lN, age);
-		cout << uDD->fN << endl << uDD->lN << endl << uDD->age << endl;
-		fND = uDD->fN;
-		lND = uDD->lN;
-		
-	}
-	delete fND;
-	delete lND;
+	cout << "\nPlease enter your first name:\n";
+	cin >> fN;
+	cout << "\nPlease enter your last name:\n";
+	cin >> lN;
+	cout << "\nPlease enter your age:\n";
+	cin >> age;
+	UserDefData* uDD = new UserDefData(fN, lN, age);
+	cout << "User Defined Data:\n";
+	cout << uDD->fN << endl << uDD->lN << endl << uDD->age << endl;
+	fND = uDD->fN;
+	lND = uDD->lN;
+	cout << "Dynamic Arrays:\n";
+	cout << fND << endl << lND << endl << age << endl;
+	delete[] fND;
+	delete[] lND;
+	
+	return 0;
 }
